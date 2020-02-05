ea7edfd71da052677838ae5923446aebf64c57cd
Vinícius <viniciusabuhid@gmail.com>
2020-02-05 16:56:43 -0300

    nao fiz o passo 3 mas sao quase 17h

--- semana4/classes/blog/blog.js
+++ semana4/classes/blog/blog.js
@@ -9,10 +9,18 @@ class post {
 }
 
 function criarPost() {
-    const titulopost = document.getElementById("titulo").value
-    const nomeautor = document.getElementById("autor").value
-    const emailautor = document.getElementById("email").value
-    const conteudopost = document.getElementById("conteudo").value
+    let titulopost = document.getElementById("titulo").value
+    let nomeautor = document.getElementById("autor").value
+    let emailautor = document.getElementById("email").value
+    let conteudopost = document.getElementById("conteudo").value
+    let campos = document.getElementsByClassName("campo").value
+    let array = []
+    
     let novoPost = new post(titulopost, nomeautor, emailautor, conteudopost)
     console.log(novoPost)
+    
+    array.push(novoPost.autor, novoPost.email, novoPost.titulo, novoPost.conteudo)
+    console.log(array)
+
+    document.getElementsByClassName("campo").value = ""
 }
