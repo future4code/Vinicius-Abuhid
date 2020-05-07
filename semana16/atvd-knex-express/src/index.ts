0147f382fc0be5ccd44a7802feac004869c6a266
Vinicius <viniciusabuhid@gmail.com>
2020-05-06 22:17:48 -0300

    na vdd, só agora

--- semana16/atvd-knex-express/src/index.ts
+++ semana16/atvd-knex-express/src/index.ts
@@ -34,10 +34,6 @@ async function getSumOfGenders(gender: string): Promise<any>{
     return result[0]
 }
 
-
-// getActorByName("Suzana Vieira")
-// getSumOfGenders('male')
-
 const updateSalary = async(salary: number, id: string):Promise<any> => {
     const result = await connection('Actor').where({id}).update({salary})
     return result
@@ -53,6 +49,7 @@ const getAvgSalaryByGender = async( gender:string): Promise<any>=> {
 }
 
 const app = express()
+app.use(express.json());
 
 app.get("/actor/?gender=", async (req: Request, res: Response) => {
     try {
@@ -81,26 +78,90 @@ app.get("/actor/?gender=", async (req: Request, res: Response) => {
       }
   })
 
-  app.post("/actor", async (req: Request, res: Response) => {
-    try {
-      await updateSalary(req.body.id, req.body.salary);
-      res.status(200).send({
-        message: "Success",
-      });
-    } catch (err) {
-      res.status(400).send({
-        message: err.message,
-      });
+  app.delete("/actor/:id", async (req: Request, res: Response)=>{
+      try{
+        await deleteActor(req.params.id)
+        res.status(200).send()
+      }
+      catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+      }
+  })
+
+
+async function createNewMovie(id: string, name: string, synopsis: string, release_date: string, 
+rating: number):Promise<any>{
+    const result = await connection.raw(`
+    INSERT INTO Movies(id, name, synopsis, release_date, rating)
+    VALUES('${id}','${name}','${synopsis}','${release_date}',${rating})
+    `)
+    return result[0]
+}
+
+app.post('/movie', async(req: Request, res: Response)=>{
+    try{
+        await createNewMovie(req.body.id, req.body.name, req.body.synopsis, 
+        req.body.release_date, req.body.rating )
+        res.status(200).send({
+            message: 'sucess'
+        })
+    }
+    catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+async function getAllMovies(): Promise<any>{
+    const result = await connection.select('*').from('movies').limit(15)
+    return result
+}
+
+app.get('/movie/all', async (req: Request, res: Response)=>{
+    try{
+        await getAllMovies()
+        res.status(200).send({
+            message: 'success'
+        })
+    }
+    catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+async function searchMovies(keyWord: string): Promise<any>{
+    const result = await connection.raw(`
+    SELECT * FROM Movies
+    WHERE name LIKE '%${keyWord}%' OR synopsis LIKE '%${keyWord}%'
+    ORDER BY release_date 
+    `)
+    return result[0]
+}
+
+app.get('/movie/search?query=', async (req: Request, res: Response) =>{
+    try{
+        const search = await searchMovies(req.query.keyWord as string)
+        res.status(200).send({
+            message: search
+        })
+    }
+    catch(err){
+        res.status(400).send({
+            message: err.message
+        })
+    }
+})
+
+const server = app.listen(process.env.PORT || 3003, () => {
+    if (server) {
+      const address = server.address() as AddressInfo;
+      console.log(`Server is running in http://localhost:${address.port}`);
+    } else {
+      console.error(`Failure upon starting server.`);
     }
   });
-
-  app.use(express.json());
-
-// const server = app.listen(process.env.PORT || 3003, () => {
-//     if (server) {
-//       const address = server.address() as AddressInfo;
-//       console.log(`Server is running in http://localhost:${address.port}`);
-//     } else {
-//       console.error(`Failure upon starting server.`);
-//     }
-//   });
