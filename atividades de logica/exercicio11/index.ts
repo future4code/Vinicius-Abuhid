2fa0285aefd8bc309ff7082b1fcd82b7baeae3bd
Vinicius <viniciusabuhid@gmail.com>
2020-06-25 16:42:44 -0300

    updates

--- atividades de logica/exercicio11/index.ts
+++ atividades de logica/exercicio11/index.ts
@@ -8,16 +8,16 @@ const rl = readline.createInterface({
 const userData = async(question:string) => {
   return new Promise((res, rej)=>{
     rl.question(question, (answer:any) => {
+      res(answer)
     });
-    rl.close()
-    res()
   })
 }
 
 const questionMaker = async()=>{
 const name = await userData('qual o seu nome?')
 const age = await userData('qual a sua idade?')
-return ('Suas informações são:' + {name,age })
+rl.close()
+console.log('Suas informações são:', {name,age })
 }
 
 console.log(questionMaker())
